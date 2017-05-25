45bbf8cf6eb6130d5edd6f8883ac26b6a5598e4b
Angular CLI <angular-cli@angular.io>
2017-05-25 13:00:00 +0530

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angular2practicePage } from './app.po';
+
+describe('angular2practice App', () => {
+  let page: Angular2practicePage;
+
+  beforeEach(() => {
+    page = new Angular2practicePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
